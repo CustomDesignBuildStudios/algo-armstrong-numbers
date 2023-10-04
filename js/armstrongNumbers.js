e9748aa63d73459287b27b1692565c48ca3d86ff
Michael Termotto <mjtermotto11@gmail.com>
2023-10-04 20:01:48 +0200

    Added py isArmstrong functions

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,7 @@
 // How can you make this more scalable and reusable later?
-
-exports.findArmstrongNumbers = function() {
-
-};
+function findArmstrongNumbers(num) {
+    stringOfNum = num.toString();
+}
+  
+  module.exports = findArmstrongNumbers;
+  
--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,19 @@
 # How can you make this more scalable and reusable later?
+def is_armstrong_number(number):
+    number_as_array = [int(x) for x in str(number)]
+    armstrong_num = 0
+    for num in number_as_array:
+        armstrong_num += num**len(number_as_array)
+    if armstrong_num == number:
+        return True
+    else:
+        return False
+    
 def find_armstrong_numbers(numbers):
+    armstrong_numbers = []
+    for num in numbers:
+        if is_armstrong_number(num):
+            armstrong_numbers.append(num)
+    return armstrong_numbers
+
+
